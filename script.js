b3db604478ac14324d74b8fa75212b11b875acef
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-21 11:26:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
+    const f = document.querySelector("[name='fname']").value;
+	const l = document.querySelector("[name='lname']").value;
 
+	alert(`${f} ${l}`);
 }
